eb7cef9bd0e4b7974d492e4a54864eb095366bdb
Katya <katyaelekina@outlook.com>
2024-11-10 15:46:57 +0400

    comm3

--- app/src/main/java/com/example/learnapp/MenuFragment.kt
+++ app/src/main/java/com/example/learnapp/MenuFragment.kt
@@ -5,6 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class MenuFragment:Fragment(R.layout.fragment_menu) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pythonImage = view.findViewById<ImageView>(R.id.python_img)
+        pythonImage.setOnClickListener {
+            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, PythonFragment())
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+    }
 }
