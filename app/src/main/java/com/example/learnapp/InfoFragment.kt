25c3f0294fcf24e3edf6aec59db15bad03fd9a10
Katya <katyaelekina@outlook.com>
2024-11-24 00:37:59 +0400

    com3

--- app/src/main/java/com/example/learnapp/InfoFragment.kt
+++ app/src/main/java/com/example/learnapp/InfoFragment.kt
@@ -1,6 +1,10 @@
 package com.example.learnapp
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +12,15 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.example.learnapp.data.data
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class InfoFragment : Fragment(R.layout.fragment_info) {
 
-    private lateinit var data: data
+    private lateinit var data: data // Убедитесь, что класс Data реализует Parcelable
 
     companion object {
         private const val ARG_DATA = "data"
@@ -40,9 +49,11 @@ class InfoFragment : Fragment(R.layout.fragment_info) {
             data = it.getParcelable(ARG_DATA) ?: return
         }
 
-        view.findViewById<TextView>(R.id.textViewName).text = data.name
-        view.findViewById<TextView>(R.id.textViewAnnotation).text = data.anat
-        view.findViewById<TextView>(R.id.textViewInfo).text = data.info
+        // Форматирование текста: замена двух или более пробелов на новую строку
+        view.findViewById<TextView>(R.id.textViewName).text = formatText(data.name)
+        view.findViewById<TextView>(R.id.textViewAnnotation).text = formatText(data.anat)
+        view.findViewById<TextView>(R.id.textViewInfo).text = formatText(data.info)
+
         val pythonImage = view.findViewById<ImageView>(R.id.back_button)
         pythonImage.setOnClickListener {
             val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
@@ -51,4 +62,10 @@ class InfoFragment : Fragment(R.layout.fragment_info) {
             fragmentTransaction.commit()
         }
     }
+
+    private fun formatText(input: String): String {
+        // Заменяем два или более пробелов на новую строку
+        return input.replace(Regex("\\s{2,}"), "\n")
+    }
+
 }
